596bcf2fe0c7d9095e5fa9b3f6b69eca4868544a
Lymanto <lymantohadibrata@gmail.com>
2020-06-29 19:12:47 +0700

    mostpicked section

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -5,6 +5,7 @@ import propTypes from "prop-types";
 export default function Button(props) {
   const className = [props.className];
   if (props.isPrimary) className.push("btn-primary");
+  if (props.isLight) className.push("btn-light");
   if (props.isLarge) className.push("btn-lg");
   if (props.isSmall) className.push("btn-sm");
   if (props.isBlock) className.push("btn-block");
@@ -16,7 +17,6 @@ export default function Button(props) {
 
   if (props.isDisabled || props.isLoading) {
     if (props.isDisabled) className.push("disabled");
-
     return (
       <span className={className.join(" ")} style={props.style}>
         {props.isLoading ? (
@@ -72,14 +72,16 @@ export default function Button(props) {
 Button.propTypes = {
   type: propTypes.oneOf(["button", "link"]),
   onClick: propTypes.func,
-  target: propTypes.string,
   href: propTypes.string,
+  target: propTypes.string,
   className: propTypes.string,
+  isPrimary: propTypes.bool,
+  isLight: propTypes.bool,
+  isExternal: propTypes.bool,
   isDisabled: propTypes.bool,
   isLoading: propTypes.bool,
   isSmall: propTypes.bool,
   isLarge: propTypes.bool,
   isBlock: propTypes.bool,
-  isExternal: propTypes.bool,
   hasShadow: propTypes.bool,
 };
