6f171ca80f068bda043eb925f18b36e2c743019d
Lymanto <lymantohadibrata@gmail.com>
2020-07-25 18:13:28 +0700

    finishing checkout page

---
+++ src/elements/Form/index.js
@@ -0,0 +1,6 @@
+import InputNumber from "./InputNumber";
+import InputDate from "./InputDate";
+import InputFile from "./InputFile";
+import InputText from "./InputText";
+
+export { InputNumber, InputDate, InputFile, InputText };
--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
+
 import ImageHero from "assets/images/img-hero.jpg";
 import ImageHero_ from "assets/images/img-hero-frame.jpg";
-import IconCities from "assets/images/icons/ic_cities.svg";
-import IconTraveler from "assets/images/icons/ic_traveler.svg";
-import IconTreasure from "assets/images/icons/ic_treasure.svg";
+
 import Button from "elements/Button";
+
 import formatNumber from "utils/formatNumber";
 
 export default function Hero(props) {
@@ -47,7 +47,7 @@ export default function Hero(props) {
                 <img
                   width="36"
                   height="36"
-                  src={IconTraveler}
+                  src="/images/ic_traveler.svg"
                   alt={`${props.data.travelers} Travelers`}
                 />
                 <h6 className="mt-3">
@@ -61,7 +61,7 @@ export default function Hero(props) {
                 <img
                   width="36"
                   height="36"
-                  src={IconTreasure}
+                  src="/images/ic_treasure.svg"
                   alt={`${props.data.treasures} Treasure`}
                 />
                 <h6 className="mt-3">
@@ -75,7 +75,7 @@ export default function Hero(props) {
                 <img
                   width="36"
                   height="36"
-                  src={IconCities}
+                  src="/images/ic_cities.svg"
                   alt={`${props.data.cities} Cities`}
                 />
                 <h6 className="mt-3">
